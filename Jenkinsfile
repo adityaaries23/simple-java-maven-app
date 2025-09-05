443b68986dde98b931190e3ddd790e1778e54bcf
aditya.aries <102455608+adityaaries23@users.noreply.github.com>
2025-09-05 13:33:53 +0700

    add manual approval stage

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,6 +3,14 @@ node {
     stage('Checkout') { git 'https://github.com/adityaaries23/simple-java-maven-app.git' }
     stage('Build') { sh 'mvn -B -DskipTests clean package' }
     stage('Test') {sh 'mvn test'}
-    stage('Deploy') {sh './jenkins/scripts/deliver.sh'}
+    stage('Manual Approval') {
+      input {
+        message: 'Lanjutkan ke tahap Deploy?'
+        ok: 'Proceed'
+      }
+    }
+    stage('Deploy') {
+        sh './jenkins/scripts/deliver.sh'
+    }
   }
 }
--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,10 +4,6 @@ echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
-if [ -f .pidfile ]; then
-  kill $(cat .pidfile) 2>/dev/null || true
-  rm .pidfile
-fi
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
@@ -29,3 +25,9 @@ echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar &
 echo $! > .pidfile
+sleep 60
+
+if [ -f .pidfile ]; then
+  kill $(cat .pidfile) 2>/dev/null || true
+  rm .pidfile
+fi
